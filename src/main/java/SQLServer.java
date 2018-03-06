255df2b6c926542c5780fca97522c3234ebb9594
Chet Mancini <chetmancini@gmail.com>
2018-03-06 15:21:00 -0500

    CM: sqlserver

---
+++ src/main/java/SQLServer.java
@@ -0,0 +1,69 @@
+
+import java.sql.*;
+
+import com.microsoft.sqlserver.jdbc.*;
+
+public class SQLServer {
+
+    private SQLServerParams params;
+
+    public SQLServer(SQLServerParams params) {
+
+        this.params = params;
+    }
+
+    public void query() {
+        Connection conn = null;
+        CallableStatement callableStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            SQLServerDataSource ds = new SQLServerDataSource();
+            ds.setUser(params.getUser());
+            ds.setPassword(params.getPassword());
+            ds.setServerName(params.getHost());
+            ds.setPortNumber(params.getPort());
+            ds.setDatabaseName(params.getDatabase());
+            conn = ds.getConnection();
+
+            // Execute a stored procedure that returns some data.
+            callableStatement = conn.prepareCall("{call dbo.uspGetEmployeeManagers(?)}");
+            callableStatement.setInt(1, 50);
+            resultSet = callableStatement.executeQuery();
+
+            while (resultSet.next()) {
+                System.out.println("EMPLOYEE: " + resultSet.getString("LastName") +
+                        ", " + resultSet.getString("FirstName"));
+                System.out.println("MANAGER: " + resultSet.getString("ManagerLastName") +
+                        ", " + resultSet.getString("ManagerFirstName"));
+                System.out.println();
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (resultSet != null) try {
+                resultSet.close();
+            } catch (Exception e) {
+            }
+
+            if (callableStatement != null) try {
+                callableStatement.close();
+            } catch (Exception e) {
+            }
+
+            if (conn != null) try {
+                conn.close();
+            } catch (Exception e) {
+            }
+
+            System.exit(1);
+        }
+    }
+
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ src/main/java/SQLServerParams.java
@@ -0,0 +1,42 @@
+
+public class SQLServerParams {
+
+    private String host;
+    private Integer port;
+    private String user;
+    private String password;
+    private String database;
+
+    public SQLServerParams(String host, Integer port, String user, String password, String database) {
+        this.host = host;
+        this.port = port;
+        this.user = user;
+        this.password = password;
+        this.database = database;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public int getPort() {
+        if (this.port == null) {
+            return 1433;
+        } else {
+            return port;
+        }
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getDatabase() {
+        return database;
+    }
+
+}
