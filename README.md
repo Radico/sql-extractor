67e9867f783edcd0cd243d0d268b2fb77d5c6339
Chet Mancini <chetmancini@gmail.com>
2018-03-15 15:10:32 -0400

    CM: rename

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# sqlserver-extractor
+# sql-extractor
 Because Python TDS isn't cutting it.
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 gradle shadowJar
-scp -i ~/git/Radico/keys/radico.pem build/libs/sqlserver-extractor-1.0-SNAPSHOT-all.jar ec2-user@ci.radi.co:sqlserver-extractor-1.0-SNAPSHOT-all.jar
+scp -i ~/git/Radico/keys/radico.pem build/libs/sql-extractor-1.0-SNAPSHOT-all.jar ec2-user@ci.radi.co:sql-extractor-1.0-SNAPSHOT-all.jar
