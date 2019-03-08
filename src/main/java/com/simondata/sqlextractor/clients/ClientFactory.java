5c4fc4e69c2bb9f4dc41527798f6dd27158026b7
Chet Mancini <chetmancini@gmail.com>
2019-03-08 10:26:02 -0500

    CM: change switch

--- src/main/java/com/simondata/sqlextractor/clients/ClientFactory.java
+++ src/main/java/com/simondata/sqlextractor/clients/ClientFactory.java
@@ -8,46 +8,37 @@ public class ClientFactory {
 
     public static SQLClient makeSQLClient(String sqlType, SQLParams params) {
         SQLClient client = null;
-        switch (sqlType) {
-            case "ATHENA":
+        SqlEngine engine = SqlEngine.byName(sqlType);
+        switch (engine) {
+            case ATHENA:
                 client = new AwsAthenaClient(params);
                 break;
-            case "AZURE":
-            case "SQLSERVER":
-            case "MSSQL":
+            case SQLSERVER:
                 client = new SQLServerClient(params);
                 break;
-            case "MARIADB":
-            case "MYSQL":
+            case MYSQL:
                 client = new MySQLClient(params);
                 break;
-            case "POSTGRES":
-            case "POSTGRESQL":
+            case POSTGRESQL:
                 client = new PostgreSQLClient(params);
                 break;
-            case "REDSHIFT":
+            case REDSHIFT:
                 client = new RedshiftClient(params);
                 break;
-            case "ORACLE":
+            case ORACLE:
                 logger.error("Oracle uses logins to retrieve artifacts. Customize your own.");
                 break;
-            case "INFORMIX":
+            case INFORMIX:
                 client = new InformixSQLClient(params);
                 break;
-            case "SNOWFLAKE":
+            case SNOWFLAKE:
                 client = new SnowflakeClient(params);
                 break;
-            case "SYBASE":
-            case "ACCESS":
-            case "INTERBASE":
-            case "FIREBIRD":
-            case "IBMDB2":
-            case "DB2":
-            case "BIGQUERY":
+            case BIGQUERY:
                 logger.error("Not supported yet.");
                 break;
             default:
-                logger.error("Invalid or Unknown DB type.");
+                logger.error("DB type not supported.");
                 break;
         }
         return client;
